34cc42ff96878d43d43f65509949b6e6a63aa139
AlbertoDiaz <carlosalberto.diazjimenez@gmail.com>
2023-03-26 19:41:57 -0500

    function operador in construction

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-alert('Prueba Connect');
+const txtOp1 = document.getElementById("op1")
+const txtoperacion = document.getElementById("operacion")
+const txtOp2 = document.getElementById("op2")
+const btnCalcular = document.getElementById("calcular")
+const pResultado = document.getElementById("resultado")
+
+btnCalcular.addEventListener('click', calcular)
+
+function calcular(){
+    const operacion = txtoperacion.value 
+
+    if(operacion == "+" || operacion == "-" ||  operacion == "*" || operacion == "/" ){
+        pResultado.innerText = "Calculo Posible"
+    }else{
+        pResultado.innerText = "Calculo Imposible"
+    }
+}
+
+// voy en el minuto 1:03:52 
